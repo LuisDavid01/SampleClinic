12c4fbbcd358c3756d4323368c6649483b63852d
Luis David <luisdavidmirandav12@gmail.com>
2025-07-10 08:13:50 -0600

    ui

--- clinica-web/src/components/Header/index.tsx
+++ clinica-web/src/components/Header/index.tsx
@@ -1,23 +1,25 @@
 import { Link } from "@tanstack/react-router"
 export const Header : React.FC= () => {
     return(
-        <header className="bg-white shadow-sm">
+        <header className="bg-white shadow-sm sticky z-60">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-between items-center py-6">
+            <div className="flex justify-between items-center sm:px-6 lg:px-8 h-20">
 
-                <div className="flex items-center space-x-2">
-                    <div className="w-8 h-8 bg-primary rounded-full flex items-center justify-center">
-                        <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197m13.5-9a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0z"></path>
-                        </svg>
-                    </div>
-                    <h1 className="text-2xl font-bold text-gray-900">FisioVida</h1>
+                <div className="flex items-center  ">
+                    <Link to={'/'} className=" text-2xl text-black font-semibold">Clinica Esteban Porras</Link>
                 </div>
 
 
-                <div className="flex space-x-4">
-                    
-                    <button className="px-4 py-2 text-primary border border-primary rounded-lg hover:bg-secondary transition-colors duration-200">
+                <div className="hidden md:flex md:justify-center space-x-4 md:space-x-8">
+                    <ul className="flex justify-between items-center space-x-4 md:space-x-8">
+                        <li>
+                        <Link to="/contacts" >Contacto</Link>
+                        </li>
+                        <li>
+                        <Link to="/admin/dashboard">admin</Link>
+                        </li>
+                    </ul>
+                    <button className="px-4 py-2 text-white border border-primary rounded-lg hover:bg-secondary transition-colors duration-200">
                         Iniciar Sesión
                     </button>
                     <button className="px-4 py-2 bg-primary text-white rounded-lg hover:bg-blue-700 transition-colors duration-200">
--- clinica-web/src/routes/__root.tsx
+++ clinica-web/src/routes/__root.tsx
@@ -2,7 +2,6 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Header } from "../components/Header";
-import '../App.css';
 import { Footer } from "../components/Footer";
 
 
