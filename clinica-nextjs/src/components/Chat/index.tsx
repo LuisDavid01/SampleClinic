7d2d8002366e72cddecee3a3dca4fb9b15ee8bc6
LuisDavid01 <luisdavidmirandav12@gmail.com>
2025-09-16 17:07:02 -0600

    feat: ws connection error ui

--- clinica-nextjs/src/components/Chat/index.tsx
+++ clinica-nextjs/src/components/Chat/index.tsx
@@ -11,12 +11,13 @@ import {
 import { MessageCircle, LogOut, Send, Clock, X, User } from "lucide-react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
+import { OfflineChat } from "../OfflineChat";
 
 
 
 export default function FloatingChat() {
 	const [isOpen, setIsOpen] = useState(false);
-
+	const [connectionStatus, setConnectionStatus] = useState<'connected' | 'error' | null>(null);
 	const wsRef = useRef<WebSocket | null>(null);
 
 	useEffect(() => {
@@ -27,8 +28,15 @@ export default function FloatingChat() {
 			if (!wsRef.current) {
 				wsRef.current = new WebSocket("ws://localhost:8080/ws");
 
+				wsRef.current.onerror = (error) => {
+					console.log("WebSocket error:", error);
+					setConnectionStatus('error');
+				};
+
+
 				wsRef.current.onopen = () => {
 					console.log("WebSocket connected");
+					setConnectionStatus('connected');
 				};
 
 				wsRef.current.onmessage = (event) => {
@@ -39,9 +47,6 @@ export default function FloatingChat() {
 					console.log("WebSocket disconnected");
 				};
 
-				wsRef.current.onerror = (error) => {
-					console.log("WebSocket error:", error);
-				};
 			}
 		} else {
 			console.log("WebSocket not supported");
@@ -97,119 +102,128 @@ export default function FloatingChat() {
 						"sm:fixed sm:inset-auto sm:bottom-24 sm:right-6 sm:w-96 sm:h-[600px] sm:rounded-lg",
 					)}
 				>
-					<CardHeader className="">
-						<div className="flex items-center gap-2">
-							<div className="flex-1">
+					{connectionStatus === 'error' ? (
+						<OfflineChat />
+					) :
+						<>
+							<CardHeader className="">
+
 								<div className="flex items-center gap-2">
-									<User className="h-4 w-4" />
-									<span className="font-medium">Recepcionista</span>
-									<div className={`w-2 h-2 rounded-full bg-green-400`} />
-								</div>
-								<p className="text-xs text-gray-500">Consulta chat</p>
-							</div>
-							<Button
-								variant="ghost"
-								size="sm"
-								onClick={handleExitChat}
-								className="flex items-center gap-1 text-text-primary hover:bg-gray-100 dark:hover:bg-gray-800"
-							>
-								<span>Salir</span>
-								<LogOut className="h-4 w-4" />
-							</Button>
-						</div>
-					</CardHeader>
-
-					<CardContent className="flex-1 px-4 overflow-hidden">
-						<ScrollArea className="h-full pr-4">
-							<div className="space-y-3">
-								{/*Estos mensajes son de prueba*/}
-								<div className={`flex justify-start`}>
-									<div
-										className={`max-w-[80%] p-3 rounded-lg text-sm bg-blue-500 text-white`}
+									<div className="flex-1">
+										<div className="flex items-center gap-2">
+											<User className="h-4 w-4" />
+											<span className="font-medium">Recepcionista</span>
+											<div className={`w-2 h-2 rounded-full bg-green-400`} />
+										</div>
+										<p className="text-xs text-gray-500">Consulta chat</p>
+									</div>
+									<Button
+										variant="ghost"
+										size="sm"
+										onClick={handleExitChat}
+										className="flex items-center gap-1 text-text-primary hover:bg-gray-100 dark:hover:bg-gray-800"
 									>
-										<p>El chat aun no esta disponible</p>
-										<div className="flex items-center gap-1 mt-1">
-											<Clock className="h-3 w-3 opacity-70" />
-											<span className="text-xs opacity-70">18:02 2025</span>
+										<span>Salir</span>
+										<LogOut className="h-4 w-4" />
+									</Button>
+								</div>
+							</CardHeader>
+
+							<CardContent className="flex-1 px-4 overflow-hidden">
+								<ScrollArea className="h-full pr-4">
+
+									<div className="space-y-3">
+										{/*Estos mensajes son de prueba*/}
+										<div className={`flex justify-start`}>
+											<div
+												className={`max-w-[80%] p-3 rounded-lg text-sm bg-blue-500 text-white`}
+											>
+												<p>El chat aun no esta disponible</p>
+												<div className="flex items-center gap-1 mt-1">
+													<Clock className="h-3 w-3 opacity-70" />
+													<span className="text-xs opacity-70">18:02 2025</span>
+												</div>
+											</div>
+										</div>
+
+										<div className={`flex justify-start`}>
+											<div
+												className={`max-w-[80%] p-3 rounded-lg text-sm bg-blue-500 text-white`}
+											>
+												<p>El chat aun no esta disponible</p>
+												<div className="flex items-center gap-1 mt-1">
+													<Clock className="h-3 w-3 opacity-70" />
+													<span className="text-xs opacity-70">18:02 2025</span>
+												</div>
+											</div>
+										</div>
+
+										<div className={`flex justify-start`}>
+											<div
+												className={`max-w-[80%] p-3 rounded-lg text-sm bg-blue-500 text-white`}
+											>
+												<p>El chat aun no esta disponible</p>
+												<div className="flex items-center gap-1 mt-1">
+													<Clock className="h-3 w-3 opacity-70" />
+													<span className="text-xs opacity-70">18:02 2025</span>
+												</div>
+											</div>
+										</div>
+
+										{/*Mensaje usuario prueba*/}
+										<div className={`flex justify-end`}>
+											<div
+												className={`max-w-[80%] p-3 rounded-lg text-sm bg-card text-text-primary`}
+											>
+												<p>mensaje de prueba del usuario</p>
+												<div className="flex items-center gap-1 mt-1">
+													<Clock className="h-3 w-3 opacity-70" />
+													<span className="text-xs opacity-70">18:02 2025</span>
+												</div>
+											</div>
+										</div>
+
+										<div className={`flex justify-start`}>
+											<div
+												className={`max-w-[80%] p-3 rounded-lg text-sm bg-blue-500 text-white`}
+											>
+												<p>El chat aun no esta disponible</p>
+												<div className="flex items-center gap-1 mt-1">
+													<Clock className="h-3 w-3 opacity-70" />
+													<span className="text-xs opacity-70">18:02 2025</span>
+												</div>
+											</div>
+										</div>
+
+										<div className={`flex justify-end`}>
+											<div
+												className={`max-w-[80%] p-3 rounded-lg text-sm bg-card text-text-primary`}
+											>
+												<p>mensaje de prueba del usuario</p>
+												<div className="flex items-center gap-1 mt-1">
+													<Clock className="h-3 w-3 opacity-70" />
+													<span className="text-xs opacity-70">18:02 2025</span>
+												</div>
+											</div>
 										</div>
 									</div>
-								</div>
 
-								<div className={`flex justify-start`}>
-									<div
-										className={`max-w-[80%] p-3 rounded-lg text-sm bg-blue-500 text-white`}
-									>
-										<p>El chat aun no esta disponible</p>
-										<div className="flex items-center gap-1 mt-1">
-											<Clock className="h-3 w-3 opacity-70" />
-											<span className="text-xs opacity-70">18:02 2025</span>
-										</div>
-									</div>
-								</div>
+								</ScrollArea>
+							</CardContent>
 
-								<div className={`flex justify-start`}>
-									<div
-										className={`max-w-[80%] p-3 rounded-lg text-sm bg-blue-500 text-white`}
-									>
-										<p>El chat aun no esta disponible</p>
-										<div className="flex items-center gap-1 mt-1">
-											<Clock className="h-3 w-3 opacity-70" />
-											<span className="text-xs opacity-70">18:02 2025</span>
-										</div>
-									</div>
-								</div>
-
-								{/*Mensaje usuario prueba*/}
-								<div className={`flex justify-end`}>
-									<div
-										className={`max-w-[80%] p-3 rounded-lg text-sm bg-card text-text-primary`}
-									>
-										<p>mensaje de prueba del usuario</p>
-										<div className="flex items-center gap-1 mt-1">
-											<Clock className="h-3 w-3 opacity-70" />
-											<span className="text-xs opacity-70">18:02 2025</span>
-										</div>
-									</div>
-								</div>
-
-								<div className={`flex justify-start`}>
-									<div
-										className={`max-w-[80%] p-3 rounded-lg text-sm bg-blue-500 text-white`}
-									>
-										<p>El chat aun no esta disponible</p>
-										<div className="flex items-center gap-1 mt-1">
-											<Clock className="h-3 w-3 opacity-70" />
-											<span className="text-xs opacity-70">18:02 2025</span>
-										</div>
-									</div>
-								</div>
-
-								<div className={`flex justify-end`}>
-									<div
-										className={`max-w-[80%] p-3 rounded-lg text-sm bg-card text-text-primary`}
-									>
-										<p>mensaje de prueba del usuario</p>
-										<div className="flex items-center gap-1 mt-1">
-											<Clock className="h-3 w-3 opacity-70" />
-											<span className="text-xs opacity-70">18:02 2025</span>
-										</div>
-									</div>
-								</div>
-							</div>
-						</ScrollArea>
-					</CardContent>
-
-					<CardFooter className=" px-3">
-						<form className="flex gap-2 w-full space-x-2">
-							<Input
-								placeholder="Escribe tu mensaje..."
-								className="flex-1 text-sm"
-							/>
-							<Button type="submit" disabled size="icon">
-								<Send className="h-4 w-4" />
-							</Button>
-						</form>
-					</CardFooter>
+							<CardFooter className=" px-3">
+								<form className="flex gap-2 w-full space-x-2">
+									<Input
+										placeholder="Escribe tu mensaje..."
+										className="flex-1 text-sm"
+									/>
+									<Button type="submit" disabled size="icon">
+										<Send className="h-4 w-4" />
+									</Button>
+								</form>
+							</CardFooter>
+						</>
+					}
 				</Card>
 			)}
 		</>
