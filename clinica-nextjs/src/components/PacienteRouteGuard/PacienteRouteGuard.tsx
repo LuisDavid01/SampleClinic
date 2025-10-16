028bb6131879f926f59e91b001912e9888053d3f
LuisDavid01 <luisdavidmirandav12@gmail.com>
2025-10-16 17:22:25 -0600

    feat: pacientes redige a admin si tiene los permisos

--- clinica-nextjs/src/components/PacienteRouteGuard/PacienteRouteGuard.tsx
+++ clinica-nextjs/src/components/PacienteRouteGuard/PacienteRouteGuard.tsx
@@ -27,6 +27,7 @@ export function PacienteRouteGuard({ children, fallback }: PacienteRouteGuardPro
 		);
 	}
 
+
 	if (!isAuthenticated) {
 		return (
 			<div className="flex items-center justify-center min-h-screen">
